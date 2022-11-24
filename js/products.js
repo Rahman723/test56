db250935dddbb87b538c99651cdb44ca73884394
Rahman723 <rahsaid723@gmail.com>
2022-11-24 10:36:33 +0400

    Final

---
+++ js/products.js
@@ -0,0 +1,98 @@
+let products = [
+    {
+        id: 1001,
+        title: "Biology",
+        location: "Dubai",
+        price: 2000,
+        image: "images/Biology.png",
+        space: 5
+    },
+    {
+        id: 1101,
+        title: "Biology",
+        location: "London",
+        price: 3000,
+        image: "images/Biology.png",
+        space: 7
+    },
+    {
+        id: 1101,
+        title: "Biology",
+        location: "Moscow",
+        price: 4000,
+        image: "images/Biology.png",
+        space: 8
+    },
+    {
+        id: 1002,
+        title: "Chemistry",
+        location: "Dubai",
+        price: 2000,
+        image: "images/Chemistry.png",
+        space: 5
+    },
+    {
+        id: 1102,
+        title: "Biology",
+        location: "London",
+        price: 3000,
+        image: "images/Chemistry.png",
+        space: 6
+    },
+    {
+        id: 1003,
+        title: "English",
+        location: "Dubai",
+        price: 2000,
+        image: "images/English.png",
+        space: 5
+    },
+    {
+        id: 1103,
+        title: "English",
+        location: "London",
+        price: 3000,
+        image: "images/English.png",
+        space: 9
+    },
+    {
+        id: 1004,
+        title: "Maths",
+        location: "Dubai",
+        price: 2000,
+        image: "images/Maths.png",
+        space: 5
+    },
+    {
+        id: 1104,
+        title: "Maths",
+        location: "London",
+        price: 3000,
+        image: "images/Maths.png",
+        space: 3
+    },
+    {
+        id: 1005,
+        title: "Physics",
+        location: "Dubai",
+        price: 2000,
+        image: "images/Physics.png",
+        space: 7
+    },
+    {
+        id: 1105,
+        title: "Physics",
+        location: "London",
+        price: 3000,
+        image: "images/Physics.png",
+        space: 6
+    },
+    {
+        id: 1205,
+        title: "Physics",
+        location: "Moscow",
+        price: 4000,
+        image: "images/Physics.png",
+        space: 9
+    },
+]
